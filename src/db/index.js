2e97ecf8efce4af5a77337bcfe5d5943f225c8a1
Kavish Ambani <kavishkkambani@gmail.com>
2024-07-08 03:20:28 +0530

    corrections, server is working

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB = async()=>{
+
+const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n mongo connected db host: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("mongo error: ",error);
-        process.exit(1);
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
     }
 }
 
