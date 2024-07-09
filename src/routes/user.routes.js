f21e38fb0e5526759c7d41c09f41b4d36aaaa423
Kavish Ambani <kavishkkambani@gmail.com>
2024-07-09 11:03:44 +0530

    user logout an middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 router.route("/register").post(
     upload.fields([
@@ -13,5 +14,8 @@ router.route("/register").post(
             maxCount: 1
         }
     ]),registerUser)
+router.route("/login").post(loginUser)
 
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 export default router
