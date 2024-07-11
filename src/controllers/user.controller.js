d903e27f69ec16c20176977ebd26aacbd41d6370
Kavish Ambani <kavishkkambani@gmail.com>
2024-07-11 11:03:26 +0530

    bug fix in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -110,7 +110,7 @@ const loginUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken }, "User logged in successfully"));
 });
 const logoutUser = asyncHandler(async (req, res) => {
-    await User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true })
+    await User.findByIdAndUpdate(req.user._id, { $unset: { refreshToken: 1 } }, { new: true })
     const options = {
         httpOnly: true,
         secure: true
