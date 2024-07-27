c51e4f135e26b430380327498ee41c4cd760542e
Kavish Ambani <kavishkkambani@gmail.com>
2024-07-27 10:57:30 +0530

    bug fix

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,9 +9,8 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType } = req.query
-    const { userId } = req.body;
-    //TODO: get all videos based on query, sort, pagination
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+    console.log(userId);
     const pipeline = [];
     // for using Full Text based search u need to create a search index in mongoDB atlas
     // you can include field mapppings in search index eg.title, description, as well
@@ -24,25 +23,25 @@ const getAllVideos = asyncHandler(async (req, res) => {
                 index: "search-videos",
                 text: {
                     query: query,
-                    path: [title, description]
+                    path: ["title", "description"] //search only on title, desc
                 }
             }
         });
     }
     if (userId) {
-    
-            if (!isValidObjectId(userId)) {
-                throw new ApiError(400, "invalid user");
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid userId");
+        }
+        pipeline.push({
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
             }
-            const id = userId
-            pipeline.push({
-                $match: {
-                    owner: new mongoose.Types.ObjectId(userId)
-                }
-            }); 
-        
+        });
     }
+    // fetch videos only that are set isPublished as true
     pipeline.push({ $match: { isPublished: true } });
+    //sortBy can be views, createdAt, duration
+    //sortType can be ascending(-1) or descending(1)
     if (sortBy && sortType) {
         pipeline.push({
             $sort: {
@@ -52,33 +51,34 @@ const getAllVideos = asyncHandler(async (req, res) => {
     } else {
         pipeline.push({ $sort: { createdAt: -1 } });
     }
-    pipeline.push({
-        $lookup: {
-            from: "users",
-            localField: "owner",
-            foreignField: "_id",
-            as: "ownerDetails",
-            pipeline: [
-                {
-                    $project: {
-                        username: 1,
-                        "avatar.url": 1
+    pipeline.push(
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1
+                        }
                     }
-                }
-            ]
-        }
-    },
+                ]
+            }
+        },
         {
             $unwind: "$ownerDetails"
         }
     )
-    const videoAggregate = await Video.aggregate(pipeline);
+    const videoAggregate = Video.aggregate(pipeline);
     const options = {
         page: parseInt(page, 10),
         limit: parseInt(limit, 10)
     };
     const video = await Video.aggregatePaginate(videoAggregate, options);
-    return res.status(200).json(new ApiResponse(200, video, "video fetched"));
+    return res.status(200).json(new ApiResponse(200, video, "Videos fetched successfully"));
 });
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description } = req.body;
@@ -346,4 +346,11 @@ const togglePublishStatus = asyncHandler(async (req, res) => {
     }
     return res.status(200).json(new ApiResponse(200, updatePublishedStatus, "published status updated"));
 });
-export { getAllVideos, publishAVideo, getVideoById, updateVideo, deleteVideo, togglePublishStatus }
+export { 
+    getAllVideos, 
+    publishAVideo, 
+    getVideoById, 
+    updateVideo, 
+    deleteVideo, 
+    togglePublishStatus 
+}
